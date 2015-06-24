f8dd21ef28e100d31e39a680a82333aa352ffb5f
Uffe Björklund <codeplanner@gmail.com>
2015-06-24 17:51:25 +0200

    first checkin

---
+++ src/XSockets.Protocol.Mqtt/Modules/Controller/MqttController.cs
@@ -0,0 +1,93 @@
+﻿namespace XSockets.Protocol.Mqtt.Modules.Controller
+{
+    using uPLibrary.Networking.M2Mqtt;
+    using uPLibrary.Networking.M2Mqtt.Messages;
+    using Core.Common.Socket;
+    using Core.Common.Socket.Event.Attributes;
+    using Core.XSocket;
+    using Plugin.Framework;
+    using Plugin.Framework.Attributes;
+    /// <summary>
+    /// Custom XSockets controller as MQTT-Broker. 
+    /// This also contains state since that is built-in to XSockets controllers.
+    /// </summary>
+    [XSocketMetadata("mqtt")]
+    public class MqttController : XSocketController
+    {
+        private MqttClient _mqttClient;
+
+        [NoEvent]
+        public void MqttOpen(MqttMsgConnect msg)
+        {
+            _mqttClient = new MqttClient(this.ProtocolInstance.Socket.Socket);
+            ((MqttManager)Composable.GetExport<IXSocketController>(typeof(MqttManager))).RegisterClient(this._mqttClient,msg);            
+        }
+
+        [NoEvent]
+        public void MqttClose(MqttMsgDisconnect msg)
+        {            
+            _mqttClient.OnMqttMsgDisconnected();            
+        }
+
+        [NoEvent]
+        public void MqttUnsubscribe(MqttMsgUnsubscribe msg)
+        {            
+            _mqttClient.OnMqttMsgUnsubscribeReceived(msg.MessageId, msg.Topics);            
+        }
+
+        [NoEvent]
+        public void MqttUnSubAck(MqttMsgUnsuback msg)
+        {
+            _mqttClient.OnMqttMsgUnsubscribed(msg.MessageId);
+        }
+
+        [NoEvent]
+        public void MqttPubComp(MqttMsgPubcomp msg)
+        {
+            _mqttClient.OnMqttMsgPublished(msg.MessageId, true);
+        }
+
+        [NoEvent]
+        public void MqttPubRel(MqttMsgPublish msg)
+        {
+            _mqttClient.OnMqttMsgPublishReceived(msg);
+        }
+
+        [NoEvent]
+        public void MqttPubAck(MqttMsgPuback msg)
+        {
+            // raise published message event
+            // (PUBACK received for QoS Level 1)
+            _mqttClient.OnMqttMsgPublished(msg.MessageId, true);            
+        }
+
+        [NoEvent]
+        public void MqttPublish(MqttMsgPublish msg)
+        {
+            //TODO: Fix this internalEvent stuff..
+            //// PUBLISH message received in a published internal event, no publish succeeded
+            //if (internalEvent.GetType() == typeof(MsgPublishedInternalEvent))
+            //    mqttClient.OnMqttMsgPublished(msg.MessageId, false);
+            //else
+            // raise PUBLISH message received event 
+            _mqttClient.OnMqttMsgPublishReceived(msg);                        
+        }
+        [NoEvent]
+        public void MqttSubAck(MqttMsgSuback msg)
+        {
+            _mqttClient.OnMqttMsgSubscribed(msg);
+        }
+        [NoEvent]
+        public void MqttSubscribe(MqttMsgSubscribe msg)
+        {            
+            // raise subscribe topic event (SUBSCRIBE message received)
+            _mqttClient.OnMqttMsgSubscribeReceived(msg.MessageId, msg.Topics, msg.QoSLevels);            
+        }
+
+        [NoEvent]
+        public void MqttPingReq(MqttMsgPingReq message)
+        {            
+            _mqttClient.Send(new MqttMsgPingResp());
+        }
+    }
+}
---
+++ src/XSockets.Protocol.Mqtt/Modules/Protocol/MqttProtocol.cs
@@ -0,0 +1,117 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using XSockets.Core.Common.Protocol;
+using XSockets.Core.Common.Utility.Logging;
+using XSockets.Plugin.Framework;
+using XSockets.Plugin.Framework.Attributes;
+using XSockets.Protocol.Mqtt.Modules.Controller;
+
+namespace XSockets.Protocol.Mqtt.Modules.Protocol
+{
+    /// <summary>
+    /// This MQTT implementation is based on the work of 
+    /// </summary>
+    [Export(typeof(IXSocketProtocol), Rewritable = Rewritable.No)]
+    public class MqttProtocol : XSocketProtocol, IMqttNetworkChannel
+    {
+        #region IMqttNetworkChannel
+        public bool DataAvailable
+        {
+            get { return Socket.Socket.Available > 0; }
+        }
+
+        public int Receive(byte[] buffer)
+        {
+            return Socket.Socket.Receive(buffer);
+        }
+
+        public int Receive(byte[] buffer, int timeout)
+        {
+            // check data availability (timeout is in microseconds)
+            if (Socket.Socket.Poll(timeout * 1000, SelectMode.SelectRead))
+            {
+                return Receive(buffer);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Send(byte[] buffer)
+        {
+            return Socket.Socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        public void Close()
+        {
+            Socket.Close();
+        }
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public override bool CanDoHeartbeat()
+        {
+            return false;
+        }
+
+        public override bool DoHandshake()
+        {
+            return true;
+        }
+        public override string HostResponse
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool Match(IList<byte> handshake)
+        {
+            if (!handshake.Any()) return false;
+            return ((handshake[0] & MqttMsgBase.MSG_TYPE_MASK) >> MqttMsgBase.MSG_TYPE_OFFSET) == MqttMsgBase.MQTT_MSG_CONNECT_TYPE;
+        }
+
+
+        public MqttController Broker { get; set; }
+        public override byte[] GetHostResponse()
+        {
+            try
+            {
+                var connectMessage = MqttMsgConnect.ParseHandshake(RawHandshake[0], (byte)MqttProtocolVersion.Version_3_1_1, this);                
+                Broker.MqttOpen(connectMessage);                               
+                return new byte[0]; 
+            }
+            catch (Exception ex)
+            {
+                // 3.1.4.2 -  The Server MUST validate that the CONNECT Packet conforms to section 3.1
+                // and close the Network Connection without sending a CONNACK if it does not conform
+                Composable.GetExport<IXLogger>().Error(ex, "MQTT CONNECT FAILED");
+                Disconnect(false);
+                return new byte[0];
+            }
+        }
+
+        public override IXSocketProtocol NewInstance()
+        {
+            return new MqttProtocol();
+        }
+
+        public MqttProtocol()
+        {
+            Broker = new MqttController
+            {
+                ProtocolInstance = this,                
+            };
+            Controllers.AddOrUpdate(Broker.Alias, Broker);
+        }        
+    }
+}
